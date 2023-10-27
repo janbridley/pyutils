82a68b501173180c043f194e31ae1b64e7f3c42f
janbridley <bradleyjenj@gmail.com>
2023-10-27 13:56:45 -0400

    Added polyhedron data

--- simutils.py
+++ simutils.py
@@ -21,3 +21,31 @@ def metropolis(ΔΕ, β, rng):
         return True
     else:
         return rng.random() < pm
+
+
+def make_simple_cubic(natoms, side_length, verbose=False):
+    # Initialize a grid for the points
+    ngridpoints = math.ceil(pow(natoms, 1 / 3))
+    spacing = side_length / (ngridpoints + 1)
+    if verbose:
+        print("Initial spacing (Ang):", spacing)
+    coords = (
+        np.array(
+            np.meshgrid(
+                np.arange(0, ngridpoints, 1),
+                np.arange(0, ngridpoints, 1),
+                np.arange(0, ngridpoints, 1),
+            )
+        )
+        .reshape(
+            3, -1
+        )  # Reshape into a (3,natoms) grid to ensure points are paired correctly
+        .T  # Transpose to the final (natoms,3) point list
+    )
+    coords = (
+        coords[0:natoms] * spacing
+    )  # Filter down to natoms and space out by spacing
+
+    if verbose:
+        print(coords[2:12].round(5))
+    return coords
